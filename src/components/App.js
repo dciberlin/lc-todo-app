594ab0c16da7205f164da6717a229ff963e9236f
Vasileios Psychas <vaspsych@gmail.com>
2019-12-16 11:37:58 +0100

    A component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/App.scss';
+import IntroText from './IntroText';
+
+function App() {
+  return (
+    <div className="app">
+      I come from the APP component.
+      <IntroText />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/IntroText.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function IntroText() {
+  return <div className="intro-text">I come from the intro-text component</div>;
+}
+
+export default IntroText;
