8755185e636eaca990ed2efc9d002af92fa09a4d
Vasileios Psychas <vaspsych@gmail.com>
2019-12-18 12:00:13 +0100

    Class components

--- src/components/ToDonesContainer.js
+++ src/components/ToDonesContainer.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import ToDoItem from './ToDoItem';
 
-const ToDonesContainer = props => {
-  const data = props.items;
+class ToDonesContainer extends React.Component {
+  render() {
+    const data = this.props.items;
 
-  const todoneItems = data.map(el => {
-    return <ToDoItem key={el.id} text={el.text} status={el.status}></ToDoItem>;
-  });
+    const todoneItems = data.map(el => {
+      return (
+        <ToDoItem key={el.id} text={el.text} status={el.status}></ToDoItem>
+      );
+    });
 
-  return (
-    <div className="todones-container">
-      {data.length > 0 && (
-        <div className="todones">
-          <h5>BACKLOG</h5>
-          {todoneItems}
-        </div>
-      )}
-    </div>
-  );
-};
+    return (
+      <div className="todones-container">
+        {data.length > 0 && (
+          <div className="todones">
+            <h5>BACKLOG</h5>
+            {todoneItems}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 export default ToDonesContainer;
--- src/components/ToDosContainer.js
+++ src/components/ToDosContainer.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import ToDoItem from './ToDoItem';
 
-const ToDosContainer = props => {
-  const data = props.items;
+class ToDosContainer extends React.Component {
+  render() {
+    const data = this.props.items;
 
-  const todoItems = data.map(el => {
-    return <ToDoItem key={el.id} text={el.text} status={el.status}></ToDoItem>;
-  });
+    const todoItems = data.map(el => {
+      return (
+        <ToDoItem key={el.id} text={el.text} status={el.status}></ToDoItem>
+      );
+    });
 
-  return (
-    <div className="todos-container">
-      {data.length > 0 && (
-        <div className="todos">
-          <h5>TODOS</h5>
-          {todoItems}
-        </div>
-      )}
-    </div>
-  );
-};
+    return (
+      <div className="todos-container">
+        {data.length > 0 && (
+          <div className="todos">
+            <h5>TODOS</h5>
+            {todoItems}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 export default ToDosContainer;
