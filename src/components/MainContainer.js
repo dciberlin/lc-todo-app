924eb399e7b1c469f0843a9b1a9983d2ad823749
Vasileios Psychas <vaspsych@gmail.com>
2020-01-16 13:11:56 +0100

    Well now it's pefect

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -10,7 +10,7 @@ class MainContainer extends React.Component {
     super(props);
     this.state = {
       items: [],
-      loading: false,
+      loading: true,
       feedback: false,
       showFriend: false
     };
